d7d28acf5023f3520db433d9ecde2e26287f3139
王哲 <2426654824@qq.com>
2024-02-28 07:19:21 +0800

    4.3新增member会员模块

--- member/src/main/java/com/example/train/member/controller/TestController.java
+++ member/src/main/java/com/example/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.train;
+package com.example.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello world1";
+        return "hello world!";
     }
 }
